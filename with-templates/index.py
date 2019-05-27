80210edbba5303b68f2838f7c422f8ccc1c1b33f
unknown <trutnv@protonmail.com>
2019-05-27 17:11:39 -0300

    with-templates/ - Teste utilizando templates e Jinja2

---
+++ with-templates/index.py
@@ -0,0 +1,15 @@
+# imports
+from flask import Flask, render_template
+
+# instancia
+app = Flask(__name__)
+
+# rota / (raíz)
+@app.route("/")
+@app.route("/<something>")
+def index(something = None):
+    # retorna o template (Flask procura na pasta 'templates' por padrão)
+    return render_template("example_template.html", something=something)
+    
+    # digite algo na URL, após o '/' para ver o resultado
+    # além disso, analise o template
