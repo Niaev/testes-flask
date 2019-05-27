ae08a89c3d1f57796f90a1ce1823720685d9cee5
unknown <trutnv@protonmail.com>
2019-05-27 17:10:06 -0300

    simple-flask/ - Teste muito simples

---
+++ simple-flask/index.py
@@ -0,0 +1,20 @@
+# imports
+from flask import Flask
+
+# instância
+app = Flask(__name__)
+
+# rota / (raíz)
+@app.route("/")
+def index():
+    # conteúdo HTML
+    page_content = """
+    <h1>simple-flask</h1>
+    <h3>Seja bem-vindo(a) a minha aplicação simples utilizando Python e Flask</h3>
+
+    <br>
+
+    <p>Com o framework Flask é possível desenvolver aplicações web com a liguagem de programação Python. Para saber mais, <a href="flask.pocoo.org">clique aqui</a></p>
+    """
+
+    return page_content
